c2bedad2f9e03517b01a35ee273fb1a40d7d3e2a
Tom Jia <tomchristian1998@gmail.com>
2022-03-31 21:34:29 +0800

    added some additions

---
+++ new porfolio/assets/script.js
@@ -0,0 +1,8 @@
+const rightWallAboutMe = document.querySelector('.right-wall-about-me');
+const rightWallAboutMeDrawer = document.querySelector('#about-me-text');
+
+rightWallAboutMeDrawer.addEventListener('click', function() {
+    rightWallAboutMe.style.display = '';
+
+    console.log('done')
+})
