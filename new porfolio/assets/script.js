b1e95247006dc374612fb5d9412ae16ccae26a6e
Tom Jia <tomchristian1998@gmail.com>
2022-04-05 13:37:26 +0800

    added the slide animation, a close button and started on the 2nd drawer

--- new porfolio/assets/script.js
+++ new porfolio/assets/script.js
@@ -1,8 +1,37 @@
-const rightWallAboutMe = document.querySelector('.right-wall-about-me');
+const rightWallAboutMe = document.querySelector('#right-wall-about-me');
 const rightWallAboutMeDrawer = document.querySelector('#about-me-text');
+const closeButton = document.querySelectorAll(".close");
+const majorSectionDrawer = document.querySelector('#folder-doc-major');
+const majorSection = document.querySelector('#right-wall-major-projects');
+
+
+for (i = 0; i < closeButton.length;i++) {
+    closeButton[i].addEventListener('click', function() {
+        //hiding all elements
+        majorSection.style.display = 'none'
+        rightWallAboutMe.style.display = 'none'
+       
+    })
+}
+
+majorSectionDrawer.addEventListener('click', function () {
+    //hiding all other elements
+    rightWallAboutMe.style.display = 'none'
+
+
+
+    majorSection.style.display = '';
+    majorSection.classList.add('slide')
+
+
+})
 
 rightWallAboutMeDrawer.addEventListener('click', function() {
-    rightWallAboutMe.style.display = '';
+    //hiding all the other elements
+    majorSection.style.display = 'none'
 
-    console.log('done')
+    rightWallAboutMe.style.display = '';
+    rightWallAboutMe.classList.add('slide')
+
+    
 })
