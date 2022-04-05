cad9098a4306cf6e58eed3f1544b5db4e6f6df3a
Tom Jia <tomchristian1998@gmail.com>
2022-04-05 18:44:15 +0800

    added the other drawers

--- new porfolio/assets/script.js
+++ new porfolio/assets/script.js
@@ -7,33 +7,45 @@ const majorSectionDrawer = document.querySelector('#folder-doc-major');
 const majorSection = document.getElementById('right-wall-major-projects');
 
 const otherSectionDrawer = document.getElementById('folder-doc-other');
+const otherSection = document.getElementById('other-work');
+
+const resumeDrawer = document.getElementById('folder-doc-resume');
+const resumeSection = document.getElementById('resume');
+
+const contactDrawer = document.getElementById('folder-doc-contact');
+const contactSection = document.getElementById('contact-info');
 
 const tunespliceButton = document.getElementById('tunesplice-button');
 const junglrButton = document.getElementById('junglr-button');
 const pro3Button = document.getElementById('pro3-button');
 const employeeTrackerButton = document.getElementById('employee-tracker-button');
+const weatherButton = document.getElementById('weather-scheduler-button');
 
 const tunespliceSection = document.getElementById('tunesplice');
 const junglrSection = document.getElementById('junglr');
 const pro3Section = document.getElementById('pro3');
 const employeeTrackerSection = document.getElementById('employee-tracker')
+const weatherSection = document.getElementById('weather-scheduler');
 
 
 
 for (i = 0; i < closeButton.length;i++) {
     closeButton[i].addEventListener('click', function() {
         //hiding all elements
-        majorSection.style.display = 'none'
-        rightWallAboutMe.style.display = 'none'
-       
+        majorSection.style.display = 'none';
+        rightWallAboutMe.style.display = 'none';
+        otherSection.style.display = 'none';
+        resumeSection.style.display = 'none';
+        contactSection.style.display = 'none';
     })
 }
 
 majorSectionDrawer.addEventListener('click', function () {
     //hiding all other elements
-    rightWallAboutMe.style.display = 'none'
-
-
+    rightWallAboutMe.style.display = 'none';
+    otherSection.style.display = 'none';
+    resumeSection.style.display = 'none';
+    contactSection.style.display = 'none';
 
     majorSection.style.display = '';
     // majorSection.classList.add('slide2')
@@ -44,10 +56,44 @@ majorSectionDrawer.addEventListener('click', function () {
 rightWallAboutMeDrawer.addEventListener('click', function() {
     //hiding all the other elements
     majorSection.style.display = 'none'
+    otherSection.style.display = 'none';
+    resumeSection.style.display = 'none';
+    contactSection.style.display = 'none';
+
+
 
     rightWallAboutMe.style.display = '';
     // rightWallAboutMe.classList.add('slide')
 })
+
+otherSectionDrawer.addEventListener('click', function () {
+    rightWallAboutMe.style.display = 'none';
+    majorSection.style.display = 'none';
+    resumeSection.style.display = 'none';
+    contactSection.style.display = 'none';
+
+
+    otherSection.style.display = '';
+})
+
+resumeDrawer.addEventListener('click', function(){
+    rightWallAboutMe.style.display = 'none';
+    majorSection.style.display = 'none';
+    otherSection.style.display = 'none';
+    contactSection.style.display = 'none';
+
+    resumeSection.style.display = '';
+})
+
+contactDrawer.addEventListener('click', function(){
+    rightWallAboutMe.style.display = 'none';
+    majorSection.style.display = 'none';
+    otherSection.style.display = 'none';
+    resumeSection.style.display = 'none';
+    
+    contactSection.style.display = '';
+})
+
 
 // -------------------------------------------- major projects section ------------------------------
 
@@ -56,6 +102,7 @@ tunespliceButton.addEventListener('click', function () {//tunesplice
     pro3Section.style.display = 'none';
     junglrSection.style.display = 'none';
     employeeTrackerSection.style.display = 'none';
+    weatherSection.style.display = 'none';
 
     tunespliceSection.style.display= '';
 
@@ -66,6 +113,7 @@ junglrButton.addEventListener('click', function() {//junglr
     tunespliceSection.style.display = 'none';
     pro3Section.style.display = 'none';
     employeeTrackerSection.style.display = 'none';
+    weatherSection.style.display = 'none';
 
     junglrSection.style.display = '';
 })
@@ -75,6 +123,7 @@ pro3Button.addEventListener('click', function () {//pro3
     tunespliceSection.style.display = 'none';
     junglrSection.style.display = 'none';
     employeeTrackerSection.style.display = 'none';
+    weatherSection.style.display = 'none';
     
     pro3Section.style.display = '';
 });
@@ -84,6 +133,23 @@ employeeTrackerButton.addEventListener('click', function () {
     tunespliceSection.style.display = 'none';
     junglrSection.style.display = 'none';
     pro3Section.style.display = 'none';
+    weatherSection.style.display = 'none';
 
     employeeTrackerSection.style.display = '';
 })
+
+weatherButton.addEventListener('click', function(){
+    tunespliceSection.style.display = 'none';
+    junglrSection.style.display = 'none';
+    pro3Section.style.display = 'none';
+    employeeTrackerSection.style.display = 'none';
+    
+    weatherSection.style.display = '';
+
+
+})
+
+function checkVH () {
+
+
+}
