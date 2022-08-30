e4aeeb43583013f45f80693897bd2531a442d0e7
tomjia98 <tomchristian1998@gmail.com>
2022-08-30 16:14:40 +0800

    working on the carosel

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import { Link, Router, Switch, NavLink } from "react-router-dom";
+import {} from "react-router";
+// TODO:  get the header to work correctly with redirects to the needed areas
+export default function header() {
+  return (
+    <header>
+      <h1>Tom Jia</h1>
+      {/* <Router>
+        <NavLink>Home</NavLink>
+        <NavLink>About Me</NavLink>
+        <NavLink>Resume</NavLink>
+        <NavLink>Contact Me</NavLink>
+      </Router> */}
+    </header>
+  );
+}
