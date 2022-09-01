38a586b16c6c87ae0fa4960702701cf983581119
tomjia98 <tomchristian1998@gmail.com>
2022-09-01 13:08:26 +0800

    starting to work on about me

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import acnestree from "../images/ancestree.png";
+import junglr from "../images/junglr.png";
+import tunesplice from "../images/TuneSplice.png";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default () => (
-  <Carousel autoPlay>
+  <Carousel autoPlay infiniteLoop>
     <div>
       <img alt="legend1" src={acnestree} />
-      <p className="legend">legend 1</p>
+      <p className="legend">
+        Ancestree, The collaborativly expandable ancestory viewer
+      </p>
     </div>
     <div>
-      <img alt="legend2" src={acnestree} />
-      <p className="legend">legend 2</p>
+      <img alt="legend2" src={junglr} />
+      <p className="legend">Junglr, The fun way to do social networking</p>
+    </div>
+    <div>
+      <img alt="legend3" src={tunesplice} />
+      <p className="legend">
+        tunesplice, A music guessing game that changes with the charts
+      </p>
     </div>
   </Carousel>
 );
